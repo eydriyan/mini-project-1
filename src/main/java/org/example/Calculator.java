7c9fcf706b01d72224d03e9b158cd3833a0ed3f3
eydriyan <115596606+eydriyan@users.noreply.github.com>
2024-06-20 08:44:18 +0800

    Added comments for the Calculator class
    Added comments for the Calculator class

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -3,21 +3,25 @@ package org.example;
 public class Calculator {
 
     public Calculator() {
-        System.out.println("=========WELCOME========");
+        System.out.println("===============WELCOME=============");
     }
 
+    // Method for addition
     public double add(double num1, double num2) {
         return num1 + num2;
     }
 
+    // Method for subtraction
     public double subtract(double num1, double num2) {
         return num1 - num2;
     }
 
+    // Methods for Multiplication
     public double multiply(double num1, double num2) {
         return num1 * num2;
     }
 
+    // Method for Division
     public double divide(double num1, double num2) throws ArithmeticException {
         if(num2 == 0) {
             throw new ArithmeticException("Cannot divide by zero");
